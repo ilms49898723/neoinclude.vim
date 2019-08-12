e217a1cc8a6c152073b99fba9f21383be176df7d
LittleBird <ilms49898723@gmail.com>
2019-08-12 13:39:30 +0900

    Use Jedi for python imports

--- rplugin/python3/deoplete/sources/file_include.py
+++ rplugin/python3/deoplete/sources/file_include.py
@@ -4,9 +4,13 @@
 # License: MIT license
 #=============================================================================
 
-from .base import Base
+import os
+import re
 
-import deoplete.util
+import jedi
+
+from deoplete.base.source import Base
+
 
 class Source(Base):
     def __init__(self, vim):
@@ -14,13 +18,41 @@ class Source(Base):
 
         self.name = 'file/include'
         self.mark = '[FI]'
+        self.events = None
         self.is_bytepos = True
         self.min_pattern_length = 0
 
     def get_complete_position(self, context):
-        return self.vim.call(
-            'neoinclude#file_include#get_complete_position', context['input'])
+        if context['filetype'] != 'python':
+            return self.vim.call(
+                'neoinclude#file_include#get_complete_position', context['input'])
+        else:
+            return len(context['input'])
 
     def gather_candidates(self, context):
-        return self.vim.call(
-            'neoinclude#file_include#get_include_files', context['input'])
+        if context['filetype'] != 'python':
+            return self.vim.call(
+                'neoinclude#file_include#get_include_files', context['input'])
+        else:
+            return self.gather_python_candidates(context)
+
+    def gather_python_candidates(self, context):
+        current_line = context.get('input', None)
+
+        if current_line is None:
+            return []
+        if not re.match('(^\s*import\s+|^\s*from\s+)', current_line):
+            return []
+
+        script = jedi.Script(current_line, 1, len(current_line), '')
+        completions = script.completions()
+
+        candidates = []
+        for completion in completions:
+            if completion.name == 'import' or completion.name == 'as':
+                continue
+            candidates.append({
+                'word': completion.name,
+                'kind': 'jedi'})
+
+        return candidates
